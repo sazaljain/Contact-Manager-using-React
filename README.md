3c2c7e493cd1f15b89146b4b5efa8fb1af1a03ea
sazaljain <sazaljain16@gmail.com>
2021-03-28 03:59:51 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-bzz32n
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-bzz32n)
---
+++ src/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style.css";
+
+//import App from "./App";
+
+function AddPersonForm() {
+  const [person, setPerson] = useState("");
+  function handleChange(e) {
+    setPerson(e.target.value);
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Add new contact"
+        onChange={handleChange}
+        value={person.name}
+      />
+      <button type="submit">Add</button>
+    </form>
+  );
+}
+
+function PeopleList(props) {
+  const arr = props.data;
+  const listItems = arr.map((val, index) => <li key={index}> {val}</li>);
+  return <ul>{listItems}</ul>;
+}
+const contacts = ["saz", "seenu", "jain"];
+const ell = (
+  <div>
+    <AddPersonForm />
+    <PeopleList data={contacts} />
+  </div>
+);
+ReactDOM.render(ell, document.getElementById("root"));
